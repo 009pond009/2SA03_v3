85f4cd4112bd4e9cdf31e54be2a423ca11c3941a
009pond009 <pond2555555@gmail.com>
2019-09-09 08:17:44 +0700

    game logic

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,8 +1,29 @@
-export default class CharacterCard extends Component {
-    render(){
-    return (
-    <div>{this.props.value}</div>
-    )
+import React from 'react';
+
+
+class CharacterCard extends React.Component{
+
+    state = {
+        active: false
     }
-   }
-   
+    
+    activate = () => {
+        this.setState({
+            active: !this.state.active
+        }) 
+
+        this.props.activationHandler(this.props.value);
+    }
+
+    render(){
+        let activeCard = this.state.active ? 'activeCard' : '';
+        let className = `card ${activeCard}`
+        return(
+            <div className= {className} onClick={this.activate}>
+               {this.props.value}
+            </div>
+        )
+    }
+}
+
+export default CharacterCard;
